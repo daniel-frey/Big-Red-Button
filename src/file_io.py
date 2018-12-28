67834aad31b33c1440c2c7a0bd5ad8f8d1eacba7
Daniel Frey <dann.frey@me.com>
2018-12-28 09:34:18 -0800

    file io and prompts written

---
+++ src/file_io.py
@@ -0,0 +1,36 @@
+def read_file(filename):
+    """Read a file and return the output.
+
+    The input will consist of the file that you would like to open.
+    The output will be the contents of the said file."""
+
+    if type(filename) is not str:
+        raise TypeError('the file must be a string')
+
+    with open('./' + filename + '.json') as f:
+        return f.read()
+
+    try:
+        with open(filename + '.json') as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError('the specified file was not found')
+    except IOError('Your file could not be read')
+
+
+def write_file(content, filename):
+    """Write a new json file"""
+
+    if type(content) is not str or type(filename) is not str:
+        raise TypeError('Please enter a valid filename.')
+
+    try:
+        with open('./' + filename + '.json', 'w') as f:
+            f.write(content)
+            return ('Your file has been created')
+    except IOError:
+        return ('Seomething went wrong, please try again')
+
+
+if __name__ == "__main__":
+    print(read_file('sterile'))
