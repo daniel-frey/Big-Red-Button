ab7c135955f070eee2539dc4d9d17551711ad970
Daniel Frey <dann.frey@me.com>
2019-01-02 10:40:10 -0800

    added json parsing

--- src/json_temp/jsonparse.py
+++ src/json_temp/jsonparse.py
@@ -1,14 +1,34 @@
 import json
 
-file_data = open('jsonparse.json').read()
-json_data = json.loads(file_data)
+ec2_data = open('ec2instance_template.json').read()
+ec2_json_data = json.loads(ec2_data)
 
-print(json_data)
+print(ec2_json_data)
 
-json_data['name'] = 'roger'
+ec2_json_data["NetworkInterfaces"][0]["Ipv6Addresses"][0]["Ipv6Address"] = ipv6_address
+ec2_json_data['KeyName'] = key_name
+ec2_json_data['SecurityGroupIds'] = []
+ec2_json_data['SecurityGroups'] = security_groups
+ec2_json_data['ImageId'] = image_id
+ec2_json_data['Placement'][0]["AvailabilityZone"] = region
 
-print(json_data)
+print(ec2_json_data)
 
-with open('jsonparsenew.json', 'w') as f:
-    json.dump(json_data, f)
+with open('ec2_instance_completed.json', 'w') as f:
+    json.dump(ec2_json_data, f, sort_keys=False, indent=4)
 
+
+rds_data = open('rdsinstance_template.json').read()
+rds_json_data = json.loads(rds_data)
+
+rds_json_data['DBName'] = db_name
+rds_json_data['DBInstanceIdentifier'] = db_instance_id
+rds_json_data['AllocatedStorage'] = db_storage
+rds_json_data['DBInstanceClass'] = db_instance_class
+rds_json_data['Engine'] = db_engine
+rds_json_data['MasterUsername'] = db_user_name
+rds_json_data['MasterUserPassword'] = db_user_password
+rds_json_data['VpcSecurityGroupIds'] = vpc_group
+
+with open('rdsinstance_template_completed.json', 'w') as f2:
+    json.dump(rds_json_data, f2, sort_keys=False, indent=4)
